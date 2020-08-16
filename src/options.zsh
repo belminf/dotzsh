62c06a99c37b8d0b6d5032d958525fbc626d2a53
Belmin Fernandez <belminf@gmail.com>
2020-08-15 23:39:37 -0400

    Adds more key bindings

--- src/options.zsh
+++ src/options.zsh
@@ -6,6 +6,12 @@ set -o noclobber
 ## Shift-Tab to accept auto-suggestion
 bindkey '^[[Z' autosuggest-execute
 
+## Up and down for history substring search
+bindkey '\eOA' history-substring-search-up
+bindkey '\eOB' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
 # Paging
 PAGER=$(command -v less)
 LESS='-XRF'
