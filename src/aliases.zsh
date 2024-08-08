a7ddc4abf0f001f4cc9ed16d3ae7fc36370725f5
Belmin Fernandez <belminf@gmail.com>
2024-08-08 00:20:09 -0400

    Removes unused aliases

--- src/aliases.zsh
+++ src/aliases.zsh
@@ -7,21 +7,6 @@ alias egrep='egrep --color=auto'
 alias rg='rg -S'
 alias tree='tree -C -I "__pycache__|*.pyc"'
 
-# Git
-alias ga='git add'
-alias gap='git add -p'
-alias gs='git st'
-alias gp='git push'
-alias gpu='git pull'
-alias gpf='git push -f'
-alias gc='git commit -m'
-alias gca='git commit -a -m'
-alias gcm='git commend'
-alias gcmm='git commend -m'
-alias gl='git lg'
-alias gd='git diff'
-alias gdc='git diff --cached'
-
 # Watch with expansion
 # Ref: https://unix.stackexchange.com/questions/25327/watch-command-alias-expansion
 alias watch='watch '
@@ -75,18 +60,6 @@ function vc() {
   nvim $(git conflicted | tr '\n' ' ')
 }
 
-function clip() {
-
-  # x11 - Arch
-  if hash xclip 2>/dev/null; then
-    xclip -selection clipboard <"$1"
-
-  # macOS
-  else
-    pbcopy <"$1"
-  fi
-}
-
 function mkcd() {
   mkdir -p "$@" && cd "$@" || return
 }
@@ -97,8 +70,4 @@ function mkproj() {
 
 function cdtmp() {
   cd "$(mktemp -d)" || return
-}
-
-function subnet_vpc() {
-  aws ec2 describe-subnets --subnet-ids "$1" | jq -r '.Subnets[0].VpcId'
 }
