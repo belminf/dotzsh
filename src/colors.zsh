b111e72cea1bafda18b7d00573d6315b219f4bbd
Belmin Fernandez <belmin.fernandez@crowdstrike.com>
2024-08-14 13:05:08 -0400

    Updates colors again

--- src/colors.zsh
+++ src/colors.zsh
@@ -15,18 +15,23 @@ EZA_COLORS="reset:"
 
 # File date
 EZA_COLORS+="da=38;5;151:"
-# My user
-EZA_COLORS+="uu=38;5;230:"
-# Root user
-EZA_COLORS+="uR=38;5;228:"
-# User RWX
-EZA_COLORS+="ur=38;5;106:uw=38;5;106:ux=38;5;106:ue=38;5;106:"
-# Group RWX
-EZA_COLORS+="gr=38;5;228:gw=38;5;228:gx=38;5;228:"
-# Others RWX
-EZA_COLORS+="tr=38;5;125:tw=38;5;125:tx=38;5;125:"
 # Punctuation
 EZA_COLORS+="xx=38;5;240:"
+
+# My user
+EZA_COLORS+="uu=38;5;230:"
+# Other user
+EZA_COLORS+="un=38;5;224:"
+# Root user
+EZA_COLORS+="uR=38;5;225:"
+
+# User RWX
+EZA_COLORS+="ur=38;5;216:uw=38;5;216:ux=38;5;216:ue=38;5;216:"
+# Group RWX
+EZA_COLORS+="gr=38;5;215:gw=38;5;215:gx=38;5;215:"
+# Others RWX
+EZA_COLORS+="tr=38;5;209:tw=38;5;209:tx=38;5;209:"
+
 # Size <1KB
 EZA_COLORS+="nb=38;5;243:"
 # Size < 1MB
@@ -37,13 +42,16 @@ EZA_COLORS+="nm=38;5;250:"
 EZA_COLORS+="ng=38;5;255:"
 # Size > 1TB
 EZA_COLORS+="nt=38;5;255:"
+
 # Git
 EZA_COLORS+="ga=38;5;105:gm=38;5;105:gd=38;5;105:gv=38;5;105:gt=38;5;105:gi=38;5;105:gc=38;5;105:"
 
 # Directory
-EZA_COLORS+="di=38;5;39:"
+EZA_COLORS+="di=38;5;75:"
+# Regular files
+EZA_COLORS+="fi=38;5;253:"
 # Temporary files
-EZA_COLORS+="tm=38;5;242:cm=38;5;242:.*=38;5;242:"
+EZA_COLORS+="tm=38;5;247:cm=38;5;247:.*=38;5;247:"
 # Scripts and code
 EZA_COLORS+="co=38;5;121:ex=38;5;121:*.sh=38;5;121:*.zsh=38;5;121:"
 # Named pipes
