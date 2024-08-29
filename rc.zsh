efd2802593aa2ceb11ddcabfb0a6d0dae677f79e
Belmin Fernandez <belmin.fernandez@crowdstrike.com>
2024-08-29 13:44:21 -0400

    Exprimenting with some fzf changes

--- rc.zsh
+++ rc.zsh
@@ -1,7 +1,7 @@
 # Oh-my-zsh configuration
 export ZSH=$HOME/.zsh/oh-my-zsh
 HYPHEN_INSENSITIVE="true"
-zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+zstyle ':omz:update' mode reminder # just remind me to update when it's time
 DISABLE_UNTRACKED_FILES_DIRTY="true"
 ZSH_CUSTOM=$HOME/.zsh/custom
 ZSH_AUTOSUGGEST_STRATEGY=(history match_prev_cmd)
@@ -11,18 +11,14 @@ ZSH_THEME=""
 
 # fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-export FZF_ALT_C_OPTS=" --walker-skip .git,node_modules,target --preview 'tree -C {}'"
-export FZF_CTRL_T_OPTS=" --walker-skip .git,node_modules,target --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
-export FZF_COMPLETION_OPTS='--border --info=inline'
 export FZF_DEFAULT_OPTS='
-  --color=fg:#cdd6f4,fg+:#b4befe,bg:#11111b,bg+:#45475a
-  --color=hl:#89b4fa,hl+:#89dceb,info:#f5e0dc,marker:#94e2d5
+  --color=fg:#cdd6f4,fg+:#d0d0d0,bg:#000000,bg+:#262626
+  --color=hl:#89b4fa,hl+:#5fd7ff,info:#f5e0dc,marker:#94e2d5
   --color=prompt:#cba6f7,spinner:#af5fff,pointer:#af5fff,header:#94e2d5
   --color=gutter:#1e1e2e,border:#313244,separator:#313244,scrollbar:#b4befe
   --color=preview-bg:#313244,label:#aeaeae,query:#cdd6f4,disabled:#842626
-  --border="sharp" --border-label="" --preview-window="border-double" --prompt=" "
-  --marker=">" --pointer="◆" --separator="─" --scrollbar="│"
-  --info="right"'
+  --preview-window="border-block" --prompt=" " --marker=">" --pointer="◆"
+  --separator="" --scrollbar="│" --layout="reverse"'
 
 _fzf_compgen_path() {
   fd --hidden --follow --exclude ".git" . "$1"
@@ -56,10 +52,9 @@ source $ZSH/oh-my-zsh.sh
 
 # fzf zsh-completion
 zstyle ':completion:*:git-checkout:*' sort false
-zstyle ':completion:*:descriptions' format '[%d]'
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu no
-zstyle ':fzf-tab:*' switch-group '<' '>'
+# zstyle ':fzf-tab:*' switch-group '<' '>'
 
 # Syntax highlighting
 fast-theme -q ~/.zsh/syntax-theme.ini
