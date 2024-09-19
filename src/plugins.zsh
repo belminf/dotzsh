9052d0b38c52a1fe1ef3fe1f26967d8b38d9166c
Belmin Fernandez <belminf@gmail.com>
2024-09-19 19:12:58 -0400

    Adds history substring search

--- src/plugins.zsh
+++ src/plugins.zsh
@@ -14,8 +14,13 @@ add_plugin zsh-autosuggestions
 # Additional ZSH completions
 add_plugin zsh-completions
 
+# Fzf history search
+add_plugin zsh-fzf-history-search
+
 # Rich syntax highlighting
 add_plugin fast-syntax-highlighting
 
-# Fzf history search
-add_plugin zsh-fzf-history-search
+# History substring search
+add_plugin zsh-history-substring-search
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
